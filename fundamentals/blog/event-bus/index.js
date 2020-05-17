db335b9357de7eb4b7572149862623f8c58286f0
Matheus <goca1377@gmail.com>
2020-05-17 15:00:23 -0300

    feat: adding services comunication with k8s

--- fundamentals/blog/event-bus/index.js
+++ fundamentals/blog/event-bus/index.js
@@ -14,10 +14,10 @@ app.post('/events', (req, res) => {
 
   events.push(event)
 
-  axios.post('http://localhost:4000/events', event)
-  axios.post('http://localhost:4001/events', event)
-  axios.post('http://localhost:4002/events', event)
-  axios.post('http://localhost:4003/events', event)
+  axios.post('http://posts-clusterip-service:4000/events', event)
+  // axios.post('http://localhost:4001/events', event)
+  // axios.post('http://localhost:4002/events', event)
+  // axios.post('http://localhost:4003/events', event)
 
   return res.send({ status: 'OK' })
 })
