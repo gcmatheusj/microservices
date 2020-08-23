b1acb4a435ec653519b360c42297ddb893877026
Matheus <goca1377@gmail.com>
2020-08-23 11:54:28 -0300

    Adding useRequest hook

--- ticketing/client/src/components/Pages/Signup/index.tsx
+++ ticketing/client/src/components/Pages/Signup/index.tsx
@@ -1,10 +1,13 @@
 import { useCallback } from 'react'
+import api from 'services/api'
 
 import Form from './Form'
 
 const Signup: React.FC = () => {
-  const onSubmit = useCallback((values) => {
-    console.log(values)
+  const onSubmit = useCallback(async (values) => {
+    const response = await api.post('/api/users/signup', values)
+
+    console.log(response.data)
   }, [])
 
   return <Form onSubmit={onSubmit} />
---
+++ ticketing/client/src/hooks/useRequest.ts
@@ -0,0 +1,14 @@
+import useSWR from 'swr'
+import api from 'services/api'
+
+const useRequest = <Data = any, Error = any>(url: string) => {
+  const { data, error, mutate } = useSWR<Data, Error>(url, async (url) => {
+    const response = await api.get(url)
+
+    return response.data
+  })
+
+  return { data, error, mutate }
+}
+
+export default useRequest
